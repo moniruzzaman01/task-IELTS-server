b52b2e25bcee1b390e5594243e8adf34e244cd09
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-06-29 12:14:52 +0600

    server code done

---
+++ index.js
@@ -0,0 +1,55 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gtpqi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const taskCollection = client.db("to-do-list").collection("all-tasks");
+
+    app.post("/tasks", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+
+    app.get("/tasks/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await taskCollection.find({ userEmail: email }).toArray();
+      res.send(result);
+    });
+
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("To Do is here. No fear");
+});
+app.listen(port, () => {
+  console.log("listen from", port);
+});
+
+// https://peaceful-waters-86091.herokuapp.com/
